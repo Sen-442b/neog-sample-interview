5359285ffc8ee76fb67940c47ac3438a5500b737
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-08-22 17:15:19 +0530

    question eight solved

---
+++ question-eight/src/main.js
@@ -0,0 +1,22 @@
+const btnShowOutput = document.getElementById("btn-show");
+const outputDiv = document.querySelector(".output");
+const BASE_API_URL = "https://api.funtranslations.com/translate/minion.json"
+const postValue = BASE_API_URL+"?text=Shubhendu"
+   
+
+
+btnShowOutput.addEventListener("click",getData)
+
+
+
+function getData(){
+    fetch(postValue)
+    .then(res=>res.json())
+    .then(data=>displayData(data));
+
+}
+
+
+function displayData(data){
+    outputDiv.innerText = (data.contents.translated).toUpperCase()
+}
